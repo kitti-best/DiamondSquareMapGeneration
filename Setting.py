cb9d2f91e31cdf37a7debcee75f7cec8998175ea
best <kittibest46@gmail.com>
2023-07-24 00:18:14 +0700

    <feat>:add global map height and width

--- Setting.py
+++ Setting.py
@@ -2,10 +2,13 @@ import pygame
 
 pygame.init()
 
-SCREENW = 513
+SCREENW = 1025
 SCREENH = 513
 HH = SCREENH/2
 HW = SCREENW/2
+
+MAPW = 65
+MAPH = 65
 
 SCREEN = pygame.display.set_mode((SCREENW,SCREENH))
 
